a7633a9d70c559420088c129946aff50c45f36ee
Jimmy-Peters <abasifreke.peters@eng.uniben.edu>
2022-07-03 08:32:01 -0700

    Task-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/**main */
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle,"
+ * use the function puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
